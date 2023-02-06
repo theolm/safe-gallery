d4d60220a3237f3a45efe4ac6b18af453c488e32
Theo <theolm.mota@gmail.com>
2023-02-06 14:13:53 -0300

    Fixing LockPage UI

--- app/src/main/java/com/theolm/safeGallery/presentation/ui/page/lock/LockPage.kt
+++ app/src/main/java/com/theolm/safeGallery/presentation/ui/page/lock/LockPage.kt
@@ -1,15 +1,21 @@
 package com.theolm.safeGallery.presentation.ui.page.lock
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Fingerprint
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.theolm.safeGallery.R
 
 @Composable
 fun LockedPage(viewModel: LockPageViewModel = hiltViewModel()) {
@@ -18,15 +24,24 @@ fun LockedPage(viewModel: LockPageViewModel = hiltViewModel()) {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        Column {
-            Text(text = "Locked")
-            Button(
-                onClick = {
-                    viewModel.askBiometric(context)
-                }
-            ) {
-                Text(text = "Unlock with fingerprint")
-            }
+        Column(
+            modifier = Modifier.clickable {
+                viewModel.askBiometric(context)
+            },
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = Icons.Default.Fingerprint,
+                contentDescription = null,
+                modifier = Modifier.size(100.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(id = R.string.app_locked),
+                style = MaterialTheme.typography.titleMedium,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
