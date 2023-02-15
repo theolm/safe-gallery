39fe2ef9f5646b0a87631adcafa69f3af29644aa
Theo <theolm.mota@gmail.com>
2023-02-15 10:10:32 -0300

    Creating temp file to save image in full res

--- app/src/main/java/com/theolm/safeGallery/presentation/ui/page/preview/PreviewPage.kt
+++ app/src/main/java/com/theolm/safeGallery/presentation/ui/page/preview/PreviewPage.kt
@@ -1,18 +1,28 @@
 package com.theolm.safeGallery.presentation.ui.page.preview
 
+import android.content.Context
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.FileProvider
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.theolm.safeGallery.BuildConfig
+import java.io.File
 
 @Destination
 @Composable
@@ -22,17 +32,27 @@ fun PreviewPage(
 ) {
     val uiState = viewModel.uiState
 
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+        if (it) {
+            viewModel.loadImage(uiState.tempUri)
+        } else {
+            navigator.popBackStack()
+        }
+    }
+
+    LaunchedEffect(true) { launcher.launch(uiState.tempUri) }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
     ) {
-        uiState.image?.let {
+        uiState.uri?.let {
             Image(
                 modifier = Modifier
                     .fillMaxSize()
                     .align(Alignment.Center),
-                bitmap = it.asImageBitmap(),
+                painter = rememberImagePainter(data = it),
                 contentDescription = "Image",
                 contentScale = ContentScale.FillWidth
             )
--- app/src/main/java/com/theolm/safeGallery/presentation/ui/page/preview/PreviewPageViewModel.kt
+++ app/src/main/java/com/theolm/safeGallery/presentation/ui/page/preview/PreviewPageViewModel.kt
@@ -1,23 +1,45 @@
 package com.theolm.safeGallery.presentation.ui.page.preview
 
+import android.app.Application
+import android.content.Context
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModel
+import com.theolm.safeGallery.BuildConfig
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
-class PreviewPageViewModel @Inject constructor() : ViewModel() {
-    var uiState by mutableStateOf(PreviewPageUiState())
+class PreviewPageViewModel @Inject constructor(
+    private val application: Application
+) : ViewModel() {
+    var uiState by mutableStateOf(PreviewPageUiState(getTmpFileUri()))
         private set
 
-    fun loadBitmap(bitmap: Bitmap) {
-        uiState = uiState.copy(image = bitmap)
+    fun loadImage(uri: Uri) {
+        uiState = uiState.copy(uri = uri)
+    }
+
+    private fun getTmpFileUri(): Uri {
+        val tmpFile = File.createTempFile("tmp_image_file", ".jpg", application.cacheDir).apply {
+            createNewFile()
+            deleteOnExit()
+        }
+        return FileProvider.getUriForFile(
+            application,
+            "${BuildConfig.APPLICATION_ID}.provider",
+            tmpFile
+        )
     }
 }
 
 data class PreviewPageUiState(
-    val image: Bitmap? = null
+    val tempUri: Uri,
+    val uri: Uri? = null,
 )
