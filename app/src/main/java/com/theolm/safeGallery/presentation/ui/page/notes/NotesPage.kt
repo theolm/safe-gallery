42eac43dc2b29b98984a771d9d9a38ec6ea3628c
Theo <theolm.mota@gmail.com>
2023-02-26 17:25:06 -0300

    Adding TopBar in the NotesPage.kt

--- app/src/main/java/com/theolm/safeGallery/presentation/ui/page/notes/NotesPage.kt
+++ app/src/main/java/com/theolm/safeGallery/presentation/ui/page/notes/NotesPage.kt
@@ -6,13 +6,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -20,7 +20,6 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.theolm.safeGallery.R
 import com.theolm.safeGallery.presentation.ui.components.BottomNavigationHeight
-import com.theolm.safeGallery.presentation.ui.components.isScrollingUp
 import com.theolm.safeGallery.presentation.ui.page.destinations.EditNotePageDestination
 import com.theolm.safeGallery.presentation.ui.page.editNote.EditNotePageNavArgs
 import com.theolm.safeGallery.presentation.ui.page.notes.component.NoteBubble
@@ -35,39 +34,45 @@ fun NotesPage(
     viewModel: NotesViewModel = hiltViewModel()
 ) {
     val noteList by viewModel.notesFlow.collectAsState(initial = listOf())
-    val screenHeightDp = LocalConfiguration.current.screenHeightDp
     val listState = rememberLazyListState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
-            .padding(bottom = BottomNavigationHeight),
+            .padding(bottom = BottomNavigationHeight)
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         containerColor = MaterialTheme.colorScheme.background,
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                expanded = listState.isScrollingUp(),
-                icon = {
-                    Icon(
-                        Icons.Outlined.Add,
-                        contentDescription = stringResource(id = R.string.add_note)
-                    )
-                },
-                text = {
-                    Text(stringResource(id = R.string.add_note))
-                },
-                onClick = {
-                    navigator.navigate(EditNotePageDestination())
+        topBar = {
+            LargeTopAppBar(
+                title = { Text(text = stringResource(id = R.string.note)) },
+                scrollBehavior = scrollBehavior,
+                actions = {
+                    IconButton(
+                        onClick = {
+                            navigator.navigate(EditNotePageDestination())
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.NoteAdd,
+                            contentDescription = stringResource(id = R.string.add_note)
+                        )
+                    }
                 }
             )
         },
-        floatingActionButtonPosition = FabPosition.End,
     ) {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .navigationBarsPadding(),
             state = listState,
-            contentPadding = PaddingValues(top = 32.dp, bottom = (screenHeightDp / 2).dp)
+            contentPadding = PaddingValues(
+                top = it.calculateTopPadding(),
+                bottom = it.calculateBottomPadding(),
+                start = 0.dp,
+                end = 0.dp
+            )
         ) {
             items(noteList) {
                 NoteBubble(
